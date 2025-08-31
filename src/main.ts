a8da0a719408926b6fa385e4f327d3524009a918
John <johnaldo.yohanan@gmail.com>
2025-08-31 19:45:23 -0300

    Corrige o problema da barra de subscritos desaparecer quando algum de seus botões é clicado no Safari.

--- src/main.ts
+++ src/main.ts
@@ -15,15 +15,27 @@ if (reaction) openReaction(document.body, reaction);
 else openHomepage(document.body);
 
 {
-	const input_containers = document.getElementsByClassName("input-container");
+	/* Adiciona a funcionalidade de "barra de ferramentas" a todos os "input-container" da página:
+	 * Faz com que a barra apareça quando o input for focado e desapareça quando o input perder o foco
+	 * Adiciona conteúdo ao input conforme os botãos da barra forem clicados
+	 */
+
+	const input_containers = document.getElementsByClassName(
+		"input-container",
+	) as HTMLCollectionOf<HTMLDivElement>;
 	for (const container of input_containers) {
 		const toolbar = container.querySelector('[role="group"]');
 		const input = container.querySelector("input");
 		if (!toolbar || !input) continue;
 
+		// Necessário para fazer com que os botões possam ser `relatedTarget` no Safari
+		for (const button of toolbar.querySelectorAll("button")) {
+			button.setAttribute("tabindex", "0");
+		}
+
 		toolbar.addEventListener("click", (event) => {
-			const button = event.target as HTMLElement | undefined;
-			if (button && button.tagName.toLowerCase() === "button") {
+			const button = event.target as HTMLDivElement | HTMLButtonElement;
+			if (button instanceof HTMLButtonElement) {
 				insertTextAtCursor(
 					input,
 					button.dataset.toInsert ?? button.textContent ?? "",
@@ -36,14 +48,11 @@ else openHomepage(document.body);
 			toolbar.classList.remove("hidden");
 		});
 		input.addEventListener("focusout", (event) => {
-			// Certifica-se que "focusout" acontece depois de "focusin" no Safari
-			setTimeout(() => {
-				const target = event.relatedTarget;
-				if (!(target instanceof Node) || !toolbar.contains(target)) {
-					toolbar.setAttribute("aria-hidden", "true");
-					toolbar.classList.add("hidden");
-				}
-			}, 0);
+			const target = event.relatedTarget;
+			if (!(target instanceof Node && toolbar.contains(target))) {
+				toolbar.setAttribute("aria-hidden", "true");
+				toolbar.classList.add("hidden");
+			}
 		});
 	}
 }
